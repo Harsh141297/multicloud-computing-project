80337898796e17a114742afe19676f267d625a2c
harshit.choudhary <harshit.choudhary@presight.ai>
2024-02-10 12:43:13 +0400

    updated database azure module

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -65,27 +65,19 @@ resource "random_string" "udacity_suffix" {
   upper   = false
 }
 
-
-resource "azurerm_app_service_plan" "udacity_app_service_plan" {
+resource "azurerm_service_plan" "udacity_app_service_plan" {
   name                = "udacity-harshit-azure-dotnet-app"
-  location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-
-  tags = {
-    environment = "udacity"
-  }
+  location            = data.azurerm_resource_group.udacity.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
 
 resource "azurerm_app_service" "udacity_app_service" {
   name                = "udacity-harshit-azure-dotnet-app"
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
-  app_service_plan_id = azurerm_app_service_plan.udacity_app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.udacity_app_service_plan.id
 
   site_config {
     dotnet_framework_version = "v4.0"
